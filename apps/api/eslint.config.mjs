64fd8de4058ce407dd230c38bd753d250f2214ca
Dellan Muchengapadare <mactunechy@gmail.com>
2026-02-26 22:07:51 +0200

    feat: update ESLint and TypeScript configurations, add workspace dependencies, and adjust Turbo settings

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
-import eslint from '@eslint/js';
+import { config as baseConfig } from '@workspace/eslint-config/base';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['eslint.config.mjs', 'dist/**'],
   },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
+  ...baseConfig,
   eslintPluginPrettierRecommended,
   {
     languageOptions: {
@@ -29,7 +28,7 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
