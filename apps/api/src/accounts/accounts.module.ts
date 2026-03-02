c6eda791116fb3304c0a1f63d078435e71d06606
Dellan Muchengapadare <mactunechy@gmail.com>
2026-03-03 01:27:45 +0200

    feat: enhance seeding functionality and integrate Prisma with accounts module

--- apps/api/src/accounts/accounts.module.ts
+++ apps/api/src/accounts/accounts.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma.module';
 import { AccountsService } from './accounts.service';
 import { AccountsController } from './accounts.controller';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AccountsController],
   providers: [AccountsService],
+  exports: [AccountsService],
 })
-export class AccountsModule {}
+export class AccountsModule { }
--- apps/api/src/seeds/seeds.service.ts
+++ apps/api/src/seeds/seeds.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { AccountsSeed } from './domains/accounts.seed';
 
 @Injectable()
-export class SeedsService {}
+export class SeedsService {
+  private readonly logger = new Logger(SeedsService.name);
+
+  constructor(private readonly accountSeed: AccountsSeed) { }
+
+  async run() {
+    this.logger.log('🌱 Running seeds...');
+    await this.accountSeed.run();
+    this.logger.log('✅ Seeds completed.');
+  }
+}
