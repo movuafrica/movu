8a221492df9518372f27970e0d7c0dfbf919d947
Dellan Muchengapadare <mactunechy@gmail.com>
2026-03-03 22:09:50 +0200

    refactor: move seed files to db directory

--- apps/api/src/db/seeds/seeds.module.ts
+++ apps/api/src/db/seeds/seeds.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { SeedsService } from './seeds.service';
 import { AccountsSeed } from './domains/accounts.seed';
-import { AccountsModule } from '../accounts/accounts.module';
+import { AccountsModule } from '../../accounts/accounts.module';
 
 @Module({
   imports: [AccountsModule],
