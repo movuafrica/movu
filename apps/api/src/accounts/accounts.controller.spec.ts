102e00f50eb3e8ee14aeb8d006c76b24981717f5
Dellan Muchengapadare <mactunechy@gmail.com>
2026-03-03 22:06:48 +0200

    feat: refactor accounts module to use drizzle-orm and update database connection handling

--- apps/api/src/accounts/accounts.controller.spec.ts
+++ apps/api/src/accounts/accounts.controller.spec.ts
@@ -1,34 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AccountsController } from './accounts.controller';
 import { AccountsService } from './accounts.service';
-import { DbService } from '../db/db.service';
+import { DB_CONNECTION } from '../db/db.connection';
 
 describe('AccountsController', () => {
   let controller: AccountsController;
 
   beforeEach(async () => {
-    const dbServiceMock = {
-      db: {
-        insert: jest.fn().mockReturnThis(),
-        select: jest.fn().mockReturnThis(),
-        update: jest.fn().mockReturnThis(),
-        delete: jest.fn().mockReturnThis(),
-        from: jest.fn().mockReturnThis(),
-        values: jest.fn().mockReturnThis(),
-        set: jest.fn().mockReturnThis(),
-        where: jest.fn().mockReturnThis(),
-        returning: jest.fn().mockResolvedValue([]),
-      },
-    } as unknown as DbService;
+    const dbMock = {
+      insert: jest.fn().mockReturnThis(),
+      select: jest.fn().mockReturnThis(),
+      update: jest.fn().mockReturnThis(),
+      delete: jest.fn().mockReturnThis(),
+      from: jest.fn().mockReturnThis(),
+      values: jest.fn().mockReturnThis(),
+      set: jest.fn().mockReturnThis(),
+      where: jest.fn().mockReturnThis(),
+      returning: jest.fn().mockResolvedValue([]),
+    };
 
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AccountsController],
       providers: [
         AccountsService,
-        {
-          provide: DbService,
-          useValue: dbServiceMock,
-        },
+        { provide: DB_CONNECTION, useValue: dbMock },
       ],
     }).compile();
 
--- apps/api/src/db/db.module.ts
+++ apps/api/src/db/db.module.ts
@@ -1,9 +1,30 @@
 import { Global, Module } from '@nestjs/common';
-import { DbService } from './db.service';
+import { DB_CONNECTION } from './db.connection';
+import { ConfigService } from '@nestjs/config';
+import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
+
+import * as  accountSchema from "../accounts/schema"
 
 @Global()
 @Module({
-  providers: [DbService],
-  exports: [DbService],
+  providers: [
+    {
+      provide: DB_CONNECTION,
+      useFactory(configService: ConfigService) {
+        const pool = new Pool({
+          connectionString: configService.getOrThrow('DATABASE_URL'),
+        })
+
+        return drizzle(pool, {
+          schema: {
+            ...accountSchema
+          }
+        })
+      },
+      inject: [ConfigService],
+    }
+  ],
+  exports: [DB_CONNECTION],
 })
 export class DbModule { }
